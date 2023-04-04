26797ea5436bc4d3721b55a4543117efe3186ff7
JoaoGiovanne <joaogiovanneetec@gmail.com>
2023-04-04 08:56:35 -0300

    primeiro commit

--- src/Editora.java
+++ src/Editora.java
@@ -3,4 +3,11 @@ public class Editora {
     String nome;
     String email;
 
+    public Editora() {
+        this.nome = "Horacio";
+    }
+
+    public Editora(String nome) {
+        this.nome = nome;
+    }
 }
--- src/LivroTeste.java
+++ src/LivroTeste.java
@@ -9,7 +9,7 @@ public class LivroTeste {
         meuLivro.autor = "J. R. Tolkien";
 
         Editora editora1 = new Editora();
-        editora1.nome = "JojoGigi";
+        //editora1.nome = "JojoGigi";
         editora1.email = "EditoraJojoGigi@gmail.com";
         meuLivro.editora = editora1;
         meuLivro.aplicarDesconto(0.1);
@@ -22,10 +22,9 @@ public class LivroTeste {
         livroFavorito.paginas = 500;
         livroFavorito.autor = "Dan Brown";
 
-        Editora editora2 = new Editora();
-        editora2.nome = "Uai";
+        Editora editora2 = new Editora("JojoGigi");
+        //editora2.nome = "Uai";
         editora2.email = "EditoraUai@gmail.com";
-        meuLivro.editora = editora2;
         livroFavorito.editora = editora2;
         livroFavorito.aplicarDesconto(0.25);
 
