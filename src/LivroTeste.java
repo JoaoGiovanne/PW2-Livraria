e9fa73015ab08989b8f07ec3f2889d859d9c0047
JoaoEGiovanne <JoaoGiovanneEtec@gmail.com>
2023-05-16 08:57:50 -0300

    Adicionando o livro fisico

--- src/LivroTeste.java
+++ src/LivroTeste.java
@@ -2,31 +2,22 @@ public class LivroTeste {
 
     public static void main(String[] args) {
 
-        Livro meuLivro = new Livro("O Hobbit", 9.99);
-        //meuLivro.nome = "O Hobbit";
+        RegistroVendas venda = new RegistroVendas();
+        LivroFisico meuLivro = new LivroFisico("O Hobbit", 9.99);
         meuLivro.setPaginas(521);
         meuLivro.setResumo("bla bla bla bla bla");
         meuLivro.setAutor("J. R. Tolkien");
-
         Editora editora1 = new Editora();
-        //editora1.nome = "JojoGigi";
         editora1.email = "EditoraJojoGigi@gmail.com";
         meuLivro.setEditora(editora1);
-        meuLivro.aplicarDesconto(0.5);
+       // meuLivro.aplicarDesconto(0.5);
 
+        venda.adicionar(meuLivro);
         meuLivro.exibirDados();
 
-//        Livro livroFavorito = new Livro("O Código Da Vinci", 99.9);
-//        //livroFavorito.nome = "O Código Da Vinci";
-//        livroFavorito.setResumo("bla bla bla bla bla");
-//        livroFavorito.setPaginas(500);
-//        livroFavorito.setAutor("Dan Brown");
-//        Editora editora2 = new Editora("JojoGigi");
-//        //editora2.nome = "Uai";
-//        editora2.email = "EditoraUai@gmail.com";
-//        livroFavorito.setEditora(editora2);
-//        livroFavorito.aplicarDesconto(0.25);
-//        livroFavorito.setTipoCapa(TipoCapaEnum.PERSONALIZADA);
+
+
+        ///////////////////////////////////////////////////////////////////////////////////////////
 
         LivroDigital livroFavorito = new LivroDigital("O Código Da Vinci", 99.9);
         livroFavorito.setResumo("bla bla bla bla bla");
@@ -34,12 +25,15 @@ public class LivroTeste {
         livroFavorito.setAutor("Dan Brown");
         livroFavorito.setMarcaDAgua("teste@teste.com");
         Editora editora2 = new Editora("JojoGigi");
-        //editora2.nome = "Uai";
         editora2.email = "EditoraUai@gmail.com";
         livroFavorito.setEditora(editora2);
-        livroFavorito.aplicarDesconto(0.25);
+        //livroFavorito.aplicarDesconto(0.25);
         livroFavorito.setTipoCapa(TipoCapaEnum.PERSONALIZADA);
 
+
+        venda.adicionar(livroFavorito);
+
         livroFavorito.exibirDados();
+        ////////////////////////////////////////////////////////////////////////////////////////////
     }
 }
